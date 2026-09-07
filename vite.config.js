a34a5301da4460713344649b8555b212c67622f4
ali <a.allahgholipc@gmail.com>
2026-09-07 13:32:44 +0330

    Set base path to /MyResume/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/MyResume/',
   plugins: [react()],
   resolve: {
     alias: {
